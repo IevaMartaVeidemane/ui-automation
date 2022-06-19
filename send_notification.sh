b29b55a4079689dc1ed78af8251b78ab27120ef7
ieva.m.veidemane <ieva.m.veidemane@testdevlab.com>
2022-06-19 21:19:25 +0300

    fixed url

--- send_notification.sh
+++ send_notification.sh
@@ -3,7 +3,7 @@
 # Read input variables
 COMPONENT=$1
 STATUS=$2
-LINK=$3
+LINK=https://discord.com/api/webhooks/988117458915295252/A1kEyUiRsoXL-0BfZnblq8jQB7rV5l7i3sYCu4auDE58ajN0rZ-MW8VtZ8uBeFbkoqO_
 
 echo $STATUS
 
@@ -12,9 +12,6 @@ MESSAGE=""$COMPONENT" failed :no_entry:"
 if [ $STATUS = 0 ]; then
     MESSAGE=""$COMPONENT" passed :white_check_mark:"
 fi
-
-# remove double qoutes
- echo "$LINK" | tr -d '"'
 
 # Send notification
 curl -X POST \
