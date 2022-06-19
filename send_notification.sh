135ef68bcd66eabe1990e59fbe2ef671e5cbd6cf
ieva.m.veidemane <ieva.m.veidemane@testdevlab.com>
2022-06-19 21:26:09 +0300

    fixed if command

--- send_notification.sh
+++ send_notification.sh
@@ -9,7 +9,7 @@ echo $STATUS
 
 # Default message
 MESSAGE=""$COMPONENT" failed :no_entry:"
-if [ $STATUS = 0 ]; then
+if [ $STATUS == 0 ]; then
     MESSAGE=""$COMPONENT" passed :white_check_mark:"
 fi
 
