0353c68e28ba221b3bb0a228ef25f2d6cae1f607
ieva.m.veidemane <ieva.m.veidemane@testdevlab.com>
2022-06-20 20:01:05 +0300

    added changes to jenkinsfile

--- send_notification.sh
+++ send_notification.sh
@@ -3,7 +3,7 @@
 # Read input variables
 COMPONENT=$1
 STATUS=$2
-LINK=https://discord.com/api/webhooks/988117458915295252/A1kEyUiRsoXL-0BfZnblq8jQB7rV5l7i3sYCu4auDE58ajN0rZ-MW8VtZ8uBeFbkoqO_
+LINK=$3
 
 echo $STATUS
 
